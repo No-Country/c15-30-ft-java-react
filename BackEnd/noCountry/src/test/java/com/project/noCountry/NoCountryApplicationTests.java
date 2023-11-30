f7dfbba8c4e6a966e5093225c556d18c6bcdba03
Alejo Kannemann <alejokannemann@gmail.com>
2023-11-30 15:53:59 -0300

    TemplateProject

---
+++ BackEnd/noCountry/src/test/java/com/project/noCountry/NoCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.noCountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
