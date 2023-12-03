c5d6c608f04a5b62a763266ebf0d88b8932d349d
Joshua Frías <metatron1495@gmail.com>
2023-12-02 22:28:30 -0600

    Add: Últimas migraciones de paises

---
+++ BackEnd/src/main/resources/db/migration/migration/V6__insert-into-table-paises(R-S-T-U).sql
@@ -0,0 +1,105 @@
+INSERT INTO paises (nombre_pais) VALUES('Reino Unido');
+INSERT INTO paises (nombre_pais) VALUES('República Centroafricana');
+INSERT INTO paises (nombre_pais) VALUES('República Checa');
+INSERT INTO paises (nombre_pais) VALUES('República del Congo');
+INSERT INTO paises (nombre_pais) VALUES('República Democrática del Congo');
+INSERT INTO paises (nombre_pais) VALUES('República Dominicana');
+INSERT INTO paises (nombre_pais) VALUES('República Sudafricana');
+INSERT INTO paises (nombre_pais) VALUES('Ruanda');
+INSERT INTO paises (nombre_pais) VALUES('Rumanía');
+INSERT INTO paises (nombre_pais) VALUES('Rusia');
+
+INSERT INTO paises (nombre_pais) VALUES('Samoa');
+INSERT INTO paises (nombre_pais) VALUES('San Cristóbal y Nieves');
+INSERT INTO paises (nombre_pais) VALUES('San Marino');
+INSERT INTO paises (nombre_pais) VALUES('San Vicente y las Granadinas');
+INSERT INTO paises (nombre_pais) VALUES('Santa Lucía');
+INSERT INTO paises (nombre_pais) VALUES('Santo Tomé y Príncipe');
+INSERT INTO paises (nombre_pais) VALUES('Senegal');
+INSERT INTO paises (nombre_pais) VALUES('Serbia');
+INSERT INTO paises (nombre_pais) VALUES('Seychelles');
+INSERT INTO paises (nombre_pais) VALUES('Sierra Leona');
+INSERT INTO paises (nombre_pais) VALUES('Singapur');
+INSERT INTO paises (nombre_pais) VALUES('Siria');
+INSERT INTO paises (nombre_pais) VALUES('Somalia');
+INSERT INTO paises (nombre_pais) VALUES('Sri Lanka');
+INSERT INTO paises (nombre_pais) VALUES('Suazilandia');
+INSERT INTO paises (nombre_pais) VALUES('Sudán');
+INSERT INTO paises (nombre_pais) VALUES('Sudán del Sur');
+INSERT INTO paises (nombre_pais) VALUES('Suecia');
+INSERT INTO paises (nombre_pais) VALUES('Suiza');
+INSERT INTO paises (nombre_pais) VALUES('Surinam');
+
+INSERT INTO paises (nombre_pais) VALUES('Tailandia');
+INSERT INTO paises (nombre_pais) VALUES('Tanzania');
+INSERT INTO paises (nombre_pais) VALUES('Tayikistán');
+INSERT INTO paises (nombre_pais) VALUES('Timor Oriental');
+INSERT INTO paises (nombre_pais) VALUES('Togo');
+INSERT INTO paises (nombre_pais) VALUES('Tonga');
+INSERT INTO paises (nombre_pais) VALUES('Trinidad y Tobago');
+INSERT INTO paises (nombre_pais) VALUES('Túnez');
+INSERT INTO paises (nombre_pais) VALUES('Turkmenistán');
+INSERT INTO paises (nombre_pais) VALUES('Turquía');
+INSERT INTO paises (nombre_pais) VALUES('Tuvalu');
+
+INSERT INTO paises (nombre_pais) VALUES('Ucrania');
+INSERT INTO paises (nombre_pais) VALUES('Uganda');
+INSERT INTO paises (nombre_pais) VALUES('Uruguay');
+INSERT INTO paises (nombre_pais) VALUES('Uzbekistán');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
