6231e67ad8d3d2cfa3324d01f14dd105128a7ca8
Joshua Frías <metatron1495@gmail.com>
2023-12-05 00:15:35 -0600

    Add: Mappeo HTTP a usuario >> Listo para testear

--- BackEnd/src/main/java/com/cocode/controller/UsuarioController.java
+++ BackEnd/src/main/java/com/cocode/controller/UsuarioController.java
@@ -3,8 +3,7 @@ package com.cocode.controller;
 import com.cocode.model.entity.Usuario;
 import com.cocode.service.IUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,20 +12,24 @@ public class UsuarioController {
     @Autowired
     private IUsuario usuarioService;
 
-    public Usuario create(Usuario usuario) {
+    @PostMapping("usuario")
+    public Usuario create(@RequestBody Usuario usuario) {
         return usuarioService.create(usuario);
     }
 
-    public Usuario update(Usuario usuario) {
+    @PutMapping("usuario")
+    public Usuario update(@RequestBody Usuario usuario) {
         return usuarioService.create(usuario);
     }
 
-    public void delete(Long id) {
+    @DeleteMapping("usuario/{id}")
+    public void delete(@PathVariable Long id) {
         Usuario usuarioDelete = usuarioService.findById(id);
         usuarioService.delete(usuarioDelete);
     }
 
-    public Usuario showById(Long id) {
+    @GetMapping("usuario/{id}")
+    public Usuario showById(@PathVariable Long id) {
         return usuarioService.findById(id);
     }
 }
