f588f95d19beadb11acbf1434949dea62bdb82b7
Joshua Frías <metatron1495@gmail.com>
2023-12-21 02:07:09 -0600

    test: probada logica de proyecto

--- BackEnd/src/controllers/dificultad.controller.js
+++ BackEnd/src/controllers/dificultad.controller.js
@@ -1,43 +1,20 @@
 const catchError = require('../utils/catchError');
 const Dificultad = require('../models/dificultad.model');
 
-const getAll = catchError(async(req, res) => {
-    const results = await Dificultad.findAll();
-    return res.json(results);
+const getAll = catchError(async (req, res) => {
+  const results = await Dificultad.findAll();
+  return res.json(results);
 });
 
-const create = catchError(async(req, res) => {
-    const result = await Dificultad.create(req.body);
-    return res.status(201).json(result);
-});
 
-const getOne = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await Dificultad.findByPk(id);
-    if(!result) return res.sendStatus(404);
-    return res.json(result);
-});
-
-const remove = catchError(async(req, res) => {
-    const { id } = req.params;
-    await Dificultad.destroy({ where: {id} });
-    return res.sendStatus(204);
-});
-
-const update = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await Dificultad.update(
-        req.body,
-        { where: {id}, returning: true }
-    );
-    if(result[0] === 0) return res.sendStatus(404);
-    return res.json(result[1][0]);
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await Dificultad.findByPk(id);
+  if (!result) return res.sendStatus(404);
+  return res.json(result);
 });
 
 module.exports = {
-    getAll,
-    create,
-    getOne,
-    remove,
-    update
+  getAll,
+  getOne,
 }
