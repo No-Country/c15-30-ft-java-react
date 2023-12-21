a285de2642cd85dd1939dd4573f62d3cfe420bf3
Joshua Frías <metatron1495@gmail.com>
2023-12-21 01:10:46 -0600

    Fix: modulos de controladores

--- BackEnd/src/controllers/tecnologias.controller.js
+++ BackEnd/src/controllers/tecnologias.controller.js
@@ -1,43 +1,43 @@
 const catchError = require('../utils/catchError');
-const Tecnologia = require('../models/Tecnologia.model');
+const Tecnologia = require('../models/tecnologia.model');
 
-const getAll = catchError(async(req, res) => {
-    const results = await Tecnologia.findAll();
-    return res.json(results);
+const getAll = catchError(async (req, res) => {
+  const results = await Tecnologia.findAll();
+  return res.json(results);
 });
 
-const create = catchError(async(req, res) => {
-    const result = await Tecnologia.create(req.body);
-    return res.status(201).json(result);
+const create = catchError(async (req, res) => {
+  const result = await Tecnologia.create(req.body);
+  return res.status(201).json(result);
 });
 
-const getOne = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await Tecnologia.findByPk(id);
-    if(!result) return res.sendStatus(404);
-    return res.json(result);
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await Tecnologia.findByPk(id);
+  if (!result) return res.sendStatus(404);
+  return res.json(result);
 });
 
-const remove = catchError(async(req, res) => {
-    const { id } = req.params;
-    await Tecnologia.destroy({ where: {id} });
-    return res.sendStatus(204);
+const remove = catchError(async (req, res) => {
+  const { id } = req.params;
+  await Tecnologia.destroy({ where: { id } });
+  return res.sendStatus(204);
 });
 
-const update = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await Tecnologia.update(
-        req.body,
-        { where: {id}, returning: true }
-    );
-    if(result[0] === 0) return res.sendStatus(404);
-    return res.json(result[1][0]);
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await Tecnologia.update(
+    req.body,
+    { where: { id }, returning: true }
+  );
+  if (result[0] === 0) return res.sendStatus(404);
+  return res.json(result[1][0]);
 });
 
 module.exports = {
-    getAll,
-    create,
-    getOne,
-    remove,
-    update
+  getAll,
+  create,
+  getOne,
+  remove,
+  update
 }
